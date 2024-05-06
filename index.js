dc7c4cd6dd124323b46b9b85419d016cd60afee1
bryceAM <bryce.malmberg@yahoo.com>
2024-05-05 22:27:26 -0700

    feat: #5 index.js insert data and rate limits code

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const cors = require('cors');
 const express = require('express');
 const server = express();
 const cron = require('node-cron');
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 const client = require('./db/client');
 const SERVER_PORT = process.env.SERVER_PORT || 4000;
 const {
     getPCGroups
 } = require('./axios-services');
+const {
+    Groups
+} = require('./db');
 
 function secureProtocol(req, res, next) {
-    const protocol = req.protocol;
+    /*
+        Checks to see if a web hosting service is using secure protocol through
+        X-Forwarded-Proto header or if I need to handle it myself. Then rejects
+        the client request if a secure protocol such as https is not used or
+        permits it if used.
+    */
+    const rawProtocol = req.get('X-Forwarded-Proto') || req.protocol;
+    const protocol = rawProtocol.substring(0, 5).toLowerCase();
 
     if (protocol === 'https') {
         next()
@@ -25,18 +36,44 @@ const secureOptions = {
     methods: 'GET'
 };
 
+// Define a rate limiter to limit request to 33 per 5 minutes from the same IP.
+const rateLimiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 33,
+    message: 'RateLimitError: too many requests.'
+});
+
 server.use(express.json());
 server.use(cors(secureOptions));
 server.use(secureProtocol);
+server.use(rateLimiter);
 server.use('/api', require('./api'));
 
 cron.schedule('0 0 * * *', async () => {
+    /*
+        Fetches groups data from a third party database at the beginning of every day
+        and inserts that data into our local database. Handles data insertion and
+        data retrieval errors if any responses return with rejections.
+    */
     try {
-        const response = await getPCGroups();
+        const groups = await getPCGroups();
+        let message = '';
 
-        
+        for (const group of groups) {
+            try {
+                await Groups.insert(group);
+            } catch (error) {
+                message.concat(error + '\n');
+            };
+        };
+
+        if (message !== '') throw Error('DataInsertionError:', message);
     } catch (error) {
-        console.error('Error retrieving data from Planning Center API:', error);
+        if (error.name === 'DataInsertionError') {
+            console.error(error);
+        } else {
+            console.error('DataRetrievalError:', error);
+        };
     };
 });
 
@@ -53,12 +90,10 @@ server.listen(SERVER_PORT, async () => {
 
 /*
 TO-DO:
-    4. In cron.schedule() in server's index.js finish sending all retrieved data from PCO API to crosspointe_db
-    5. In db/groups.js finish writing the insert() query for updating data in crosspointe_db
     6. write any necessary production scripts in package.json
     7. write any necessary development scripts in package.json
-    8. write rate limit code in to the API to avoid ddos attacks from the website's URL
     9. write any other security features in for the API after consulting with any relevant sources on security about my specific situation
-    10. introduce rate limits on the requests from whatever ip to my server's api
-    11. restrict requests to specific endpoints
-*/
+    12. write a script/module init_db that creates the tables of the database
+    13. might need to write input validation for .post() requests if in the future handling
+        input from forms
+    */
